39bb45886f384d42d83009ad6fcaba232d9ca8d8
itoaki <itoaki.planet@gmail.com>
2019-03-10 01:53:18 +0900

    機能実装は完了

---
+++ src/views/Terms.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="terms">
+    <h1>利用規約</h1>
+    <p>利用規約の文章</p>
+    <v-calendar></v-calendar>
+    <router-link :to="{name: 'top'}">TOPに戻る</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'terms',
+}
+</script>
