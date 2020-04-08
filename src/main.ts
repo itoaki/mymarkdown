7003194e7c47b4bd12086e8afa1a776d20967bc7
ItoAkihiro <hatarakanai.ltd@gmail.com>
2020-04-08 23:28:27 +0900

    add new firebase setting

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -11,7 +12,19 @@ import './scss/style.scss';
 Vue.config.productionTip = false;
 Vue.use(Vuetify, {
   iconfont: 'fa' || 'mdi',
- });
+});
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAOxGaFAVkWmG98s9dEwv1J42D0n_h71oU',
+  authDomain: 'itoaki-markdown.firebaseapp.com',
+  databaseURL: 'https://itoaki-markdown.firebaseio.com',
+  projectId: 'itoaki-markdown',
+  storageBucket: 'itoaki-markdown.appspot.com',
+  messagingSenderId: '1110639874',
+  appId: '1:1110639874:web:b9ad1e75c6ec7ecc012e1b',
+  measurementId: 'G-7V3BPVZQXS',
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
