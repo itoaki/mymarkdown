cc181bd2671976d7f9e7dd6c44264d56d9b8a6f7
ItoAKihiro <38889755+itoaki@users.noreply.github.com>
2020-05-06 18:34:06 +0900

    不要なものを整理したりする (#12)
    不要な物などを削除し、且つプロジェクトの体裁を整えた

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,31 @@
 <template>
-  <div id="home">
-    <v-content>
-      <v-container>
-        <v-layout row>
-          <v-flex md24>
-            <img alt="MyMarkDown" src="../assets/logo.png">
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex md24>
-            <!-- お知らせ窓を作る -->
-            <v-textarea
-              prepend-inner-icon="comment"
-              label="お知らせ"
-              height=100
-              :value="value"
-              auto-grow
-              disabled
-            ></v-textarea>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex md24>
-            <v-btn color="info" @click="googleLogin">Googleアカウントでログイン</v-btn>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+<div class="home">
+  <div>
+    <img alt="MyMarkDown" src="../assets/logo.png">
   </div>
+  <p class="concept">すぐに使える・簡単に使える<br>マークダウンエディタ</p>
+  <div class="detail">
+    <p style="font-size: 20px;">
+      My Markdownはリアルタイムプレビューができる、シンプルなマークダウンエディタです。<br>
+      Googleアカウントがあればすぐに使用することができます。
+    </p>
+    <div class="infomation">
+      <お知らせ><br>
+      ・"Control + s"(Macの場合は"Command + s")で保存できるようになりました。<br>
+      ・"Alt + ↑↓"(Macの場合は"Option + ↑↓")でメモの切り替え<br>
+      ・"Control + Backspace"(Macの場合は"Command + delete")でメモの削除<br>
+    </div>
+  </div>
+
+  <br>
+  <div>
+    <button @click="googleLogin">Googleアカウントでログイン</button>
+  </div>
+<!--
+  <button @click="githubLogin">GitHubアカウントでログイン</button>
+  <button @click="twitterLogin">Twitterアカウントでログイン</button>
+-->
+</div>
 </template>
 
 <script>
@@ -42,13 +40,36 @@ export default {
   },
   methods: {
     googleLogin() {
-      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+      firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
     },
+    // twitterLogin() {
+    //   firebase.auth().signInWithPopup(new firebase.auth.TwitterAuthProvider());
+    // },
+    // githubLogin() {
+    //   firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider());
+    // },
   },
 };
 </script>
 <style lang="scss" scoped>
-  .row {
-    text-align:center;
+  .home {
+    width: 100%;
+    text-align: center;
+  }
+  .concept {
+    color: #696969;
+    margin: 10px;
+    font-size: 50px;
+  }
+
+  .detail {
+    background-color: rgba(255,255,255,0.7);
+  }
+
+  .infomation {
+    text-align: left;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
